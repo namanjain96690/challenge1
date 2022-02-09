43792bece60dc069246068c28a9910e6347441c6
Naman Jain <jain96690@gmail.com>
2022-02-09 15:27:24 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+ <>
+ <h1> Naman Selecting!!! </h1>
+ <p> My favourite series on netflix are. </p>
+ <ol>
+   <li>Breaking Bad</li>
+   <li>Money Heist</li>
+   <li>Peaky Blinders</li>
+   <li>Stranger Things</li>
+   <li>The Big Bang Theory</li>
+ </ol>
+ </>,
+  document.getElementById('root')
+);
